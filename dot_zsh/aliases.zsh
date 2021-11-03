85f66030ae9f62471ad266effb53f63f92e7ef4c
Patrick Motard <motard19@gmail.com>
2021-11-03 01:05:34 -0500

    added some aliases and paths

--- dot_zsh/aliases.zsh
+++ dot_zsh/aliases.zsh
@@ -1,2 +1,12 @@
 alias sz="source $ZDOTDIR/.zshrc"
+alias csz="cat $ZDOTDIR/.zshrc"
+
+alias szenv="source ~/.zshenv"
+alias czenv="source ~/.zshenv"
+
 alias moi="chezmoi"
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
