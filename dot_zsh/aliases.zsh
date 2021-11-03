c570b270f46f404b81bd7dc1f0a71e98234df736
Patrick Motard <motard19@gmail.com>
2021-11-03 01:12:21 -0500

    added aliases

--- dot_zsh/aliases.zsh
+++ dot_zsh/aliases.zsh
@@ -1,12 +1,20 @@
+# ~/.zsh/.zshrc
 alias sz="source $ZDOTDIR/.zshrc"
 alias csz="cat $ZDOTDIR/.zshrc"
 
+# ~/.zshenv
 alias szenv="source ~/.zshenv"
 alias czenv="source ~/.zshenv"
 
-alias moi="chezmoi"
-
+# Directories
 alias ..="cd .."
 alias ...="cd ../.."
 alias ....="cd ../../.."
 alias .....="cd ../../../.."
+
+# Git
+# nice shorthand,plus it gets rid of annoyingly launching ghostscript on accident.
+alias gs="git status"
+
+# Programs
+alias moi="chezmoi"
